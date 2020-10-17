dc53bd3d00f6a2c7fb26b602b2cdfa3d4600da9f
leszeof <literature198@gmail.com>
2020-10-17 20:59:35 +0400

    feat: resolve task morse-decoder

--- src/index.js
+++ src/index.js
@@ -35,10 +35,51 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    'space':  ' ',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) { // пришла строка
+    
+    let chars = []; 
+    for (let i = 0; i < expr.length; i = i + 10) { 
+        chars.push(expr.slice(i, i + 10));
+    }
+    //console.log(chars)
+    // набиваем chars (массив, внутри каждой ячейки 10 цифр 0 и 1 (каждая ячейка = буква))
+
+    let newArr = []; // этот массив мы забьем морзянкой (каждая ячейка = буква)
+    let newStr = '';
+
+    for (let i = 0; i < chars.length; i++) { // перебирает ячейки массива (1-2-3)
+        newStr = '';
+        //console.log(chars[i])
+        // ячейка i длиной 10 цифр уходит внутрь
+        for (let j = 0; j < 10; j = j + 2) { // идет по ячейке массива
+            //console.log(arr[i].length)
+            //console.log(chars[i].slice(j, j + 2));
+             if (chars[i].includes('*')) {
+                newStr = newStr + 'space';
+                break;
+            } else if (chars[i].slice(j, j + 2) == '10') {
+                newStr = newStr + '.';
+                //console.log(newStr);
+            } else if (chars[i].slice(j, j + 2) == '11') {
+                newStr = newStr + '-';
+                //console.log('tyt');
+            } else {
+                
+            }
+            
+        }
+        newArr.push(newStr);
+        
+    }
+    //console.log(newStr);
+    //console.log(newArr);
+    let resultArray = [];
+    newArr.forEach((word) => resultArray.push(MORSE_TABLE[word]));
+    //console.log(resultArray);
+    return resultArray.join('');
 }
 
 module.exports = {
